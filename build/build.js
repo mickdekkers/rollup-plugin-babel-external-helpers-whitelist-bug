64330368e140a99c5c710672b5b283a3c412af6d
Mick Dekkers <mickdekkersnl@gmail.com>
2017-08-31 23:39:06 +0200

    Initial commit

---
+++ build/build.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const path = require('path')
+const rollup = require('rollup')
+const babel = require('rollup-plugin-babel')
+
+const build = async () => {
+  const bundle = await rollup.rollup({
+    input: path.resolve(__dirname, '../src/foo.js'),
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        /* Set the `externalHelpersWhitelist` value
+         * to an empty array or an array containing
+         * an invalid helper name to cause the error
+         * 
+         * Try e.g. [`_typeof`] or []
+         */
+        externalHelpersWhitelist: ['_typeof']
+      })
+    ]
+  })
+
+  const { code } = await bundle.generate({
+    format: 'cjs'
+  })
+
+  fs.writeFileSync(path.resolve(__dirname, `../dist/foo.js`), code)
+}
+
+build()
